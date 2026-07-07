f3182354cfd52a7f739a18bb9a599cbf7fad4b48
Khakim Bakhramov <khakimbakhramov@gmail.com>
2026-07-07 14:46:17 +0500

    feat: save search to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
 import { Search } from './components/Search.tsx'
+import { People } from './components/People.tsx'
+import type { PersonType } from './components/Person.tsx'
+import type { ResultType } from './type.ts'
 import { Component } from 'react'
+
+const URL = 'https://swapi.dev/api/people'
 
 type State = {
   search: string
+  people: PersonType[]
+  loading: boolean
+  error: null | string
 }
 
 class App extends Component {
   state: State = {
     search: '',
+    people: [],
+    loading: true,
+    error: null,
   }
 
   handleSearch = (search: string) => this.setState({ search })
+
+  componentDidMount() {
+    this.fetchUsers()
+  }
+
+  fetchUsers = async () => {
+    try {
+      const response = await fetch(URL)
+      const { results }: ResultType = await response.json()
+      this.setState({ people: results, loading: false })
+    } catch {
+      this.setState({ error: 'Something went wrong', loading: false })
+    }
+  }
 
   render() {
     return (
       <>
         <Search search={this.state.search} onChange={this.handleSearch} />
+        <People people={this.state.people} />
       </>
     )
   }
---
+++ src/components/People.tsx
@@ -0,0 +1,59 @@
+import { Component } from 'react'
+import { Person } from './Person.tsx'
+import type { PersonType } from './Person.tsx'
+
+type PeopleProps = {
+  people: PersonType[]
+}
+
+export class People extends Component<PeopleProps> {
+  render() {
+    return (
+      <div className="container-fluid mt-4">
+        <div className="row">
+          <div className="col">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Height</th>
+                  <th scope="col">Mass</th>
+                  <th scope="col">Hair color</th>
+                  <th scope="col">Skin color</th>
+                  <th scope="col">Eye color</th>
+                  <th scope="col">Birth year</th>
+                  <th scope="col">Gender</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.people.map(
+                  ({
+                    name,
+                    height,
+                    mass,
+                    hair_color,
+                    skin_color,
+                    eye_color,
+                    birth_year,
+                    gender,
+                  }) => (
+                    <Person
+                      name={name}
+                      height={height}
+                      mass={mass}
+                      hair_color={hair_color}
+                      skin_color={skin_color}
+                      eye_color={eye_color}
+                      birth_year={birth_year}
+                      gender={gender}
+                    />
+                  ),
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
