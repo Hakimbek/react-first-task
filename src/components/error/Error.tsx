9a322ef2331589f44f264359ac94f74e6dfd2404
Khakim Bakhramov <kh.bakhramov@devsymfony.io>
2026-07-22 21:53:51 +0500

    feat: rewrite error

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,11 +1,7 @@
-import { Component } from 'react'
-
-export class Error extends Component {
-  render() {
-    return (
-      <div className="position-absolute w-100 top-0 bottom-0 d-flex justify-content-center align-items-center font-monospace text-danger">
-        <h2>Something went wrong</h2>
-      </div>
-    )
-  }
+export const Error = () => {
+  return (
+    <div className="position-absolute w-100 top-0 bottom-0 d-flex justify-content-center align-items-center font-monospace text-danger">
+      <h2>Something went wrong</h2>
+    </div>
+  )
 }
