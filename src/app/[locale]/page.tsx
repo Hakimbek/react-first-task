a4499e7bcb4eb6f44d1fc43aa0cbb850849508c4
Khakim Bakhramov <kh.bakhramov@devsymfony.io>
2026-08-16 18:35:19 +0500

    feat: add ssr for initial search page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,30 @@
 import { Suspense } from 'react'
-import { Home } from '../../layouts/home/Home.tsx'
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
+import { Home, API_URL } from '../../layouts/home/Home.tsx'
+import { getPeople } from '../../services/getPeople.ts'
+import type { ResultType } from '../../type.ts'
 
-export default function Page() {
+type Props = {
+  searchParams: Promise<{ search?: string; page?: string }>
+}
+
+export default async function Page({ searchParams }: Props) {
+  const { search = '', page = '1' } = await searchParams
+  const pageNum = Number(page)
+
+  const queryClient = new QueryClient()
+  const params = new URLSearchParams({ search, page })
+
+  await queryClient.prefetchQuery({
+    queryKey: ['people', search, pageNum],
+    queryFn: () => getPeople<ResultType>(`${API_URL}/?${params}`),
+  })
+
   return (
-    <Suspense>
-      <Home />
-    </Suspense>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense>
+        <Home />
+      </Suspense>
+    </HydrationBoundary>
   )
 }
